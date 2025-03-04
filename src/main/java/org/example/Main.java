e178238f8301db438767a331f7602a06d9f30431
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-03-04 13:13:09 +0800

    demo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
-import java.io.IOException;
 
 /**
  * @author 梁懿豪
@@ -24,17 +23,17 @@ public class Main {
         System.out.println("连接状态：" + zk.getState());
 
         // 2. 创建持久节点（需检查节点是否存在）
-        String nodePath = "/demo_node";
+        String nodePath = "/demo_node/shi";
         // 检查节点是否存在
         if (zk.exists(nodePath, false) == null) {
             // 创建节点
-            zk.create(nodePath, "Hello ZK".getBytes(),
+            zk.create(nodePath, "shitforyou".getBytes(),
                     ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             System.out.println("节点创建成功");
         }
 
         // 3. 读取节点数据
-        byte[] data = zk.getData(nodePath, false, null);
+        byte[] data = zk.getData(nodePath, true, null);
         System.out.println("节点数据：" + new String(data));
 
         // 4. 关闭连接
